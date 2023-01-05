a2d252948a7e337f5d109db45508538249bc1282
Niko <niko0728.it@gmail.com>
2023-01-06 09:20:52 +1100

    Using template

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use("/api", Routes);
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("App listening to: " + port);
+  console.log("App listening to: http://localhost:" + port);
 });
