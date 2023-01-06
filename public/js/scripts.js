17d2f44f8ef120c1e60ea03c2577c7dd851c5912
DaGoblin <s222574652@deakin.edu.au>
2023-01-06 16:48:49 +1100

    Commit for Image Preview before doing server side Code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,7 +6,31 @@ $(document).ready(function () {
         submitAddItem();
     });
     $("input#itemName, textarea#itemDescription").characterCounter();
+    $("#img-upload").on("change", imgPreview);
 });
+
+//This Function creates the preview images for the addItem upload.
+function imgPreview(e) {
+    let files = e.target.files,
+        filesLength = files.length;
+    for (let i = 0; i < filesLength; i++) {
+        let f = files[i];
+        let fileReader = new FileReader();
+        fileReader.onload = function (e) {
+            let file = e.target;
+            $(`<span class="pip">
+               <img class="imageThumb" src="${e.target.result}" title=" ${file.name}">
+               <br><a class="remove" href="#">Remove image</a></span>`).insertAfter(
+                "#img-preview"
+            );
+            $(".remove").click(function () {
+                $(this).parent(".pip").remove();
+            });
+        };
+        fileReader.readAsDataURL(f);
+    }
+    console.log(files);
+}
 
 const submitAddItem = () => {
     let formData = {};
