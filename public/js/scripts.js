1eade48785fbe947549845c674efa4acbed25a6e
Kachi-Okorie <51110360+Kachi-Okorie@users.noreply.github.com>
2023-01-07 18:30:13 +1100

    4script.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,8 @@
-const resetToken = require("../services/service")
+const resetToken = require("../../services/service")
 const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 
-document.getElementById("token").value = resetToken;
+// document.getElementById("token").value = resetToken;
+
+
+    document.forms['myForm']['token'].value = resetToken;
