402f56f6b9d2a2b18757770d7e0f6a7432694304
DaGoblin <s222574652@deakin.edu.au>
2023-01-10 15:41:52 +1100

    Server.js Cleanup

--- server.js
+++ server.js
@@ -32,38 +32,6 @@ app.use(passport.session());
 //Routes
 app.use("/", require("./routes/login"));
 
-// app.get("/", function (req, res) {
-//   res.render("index");
-// });
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log("Server has started at port " + PORT));
-
-// var express = require("express")
-// var app = express()
-// var cors = require("cors")
-// let dbConnect = require("./dbConnect");
-// let projectRoutes = require("./routes/projectRoutes");
-
-// app.use(express.static(__dirname + '/public'))
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cors())
-// app.use('/api/projects', projectRoutes)
-
-// app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next)
-// {
-//     var firstNumber = parseInt(req.params.firstNumber)
-//     var secondNumber = parseInt(req.params.secondNumber)
-//     var result = firstNumber + secondNumber || null
-//     if(result == null) {
-//       res.json({result: result, statusCode: 400}).status(400)
-//     }
-//     else { res.json({result: result, statusCode: 200}).status(200) }
-// })
-
-// var port = process.env.port || 8080;
-// app.listen(port, () => {
-//     console.log("App listening to http://localhost:" + port)
-// })
