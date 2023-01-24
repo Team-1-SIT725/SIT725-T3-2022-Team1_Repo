04677e8999ff2c7e4b5567a9ed01b665e5689de2
DaGoblin <s222574652@deakin.edu.au>
2023-01-24 15:39:33 +1100

    Save before change

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,15 @@
 var express = require("express");
 const controllers = require("../controllers");
-var router = express.Router();  
+var router = express.Router();
 
-router.get("/", (req, res) => {
+// router.get("/", (req, res) => {
+//     //controller.itemController.addItem(req, res);
+//     controllers.profileController.profileUser(req, res);
+// });
+
+router.get("/:userID?", (req, res) => {
     //controller.itemController.addItem(req, res);
-    controllers.profileController.profileUser(req,res);
+    controllers.profileController.profileUser(req, res);
 });
-
-
-
-
 
 module.exports = router;
