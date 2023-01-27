07aed5cc01ab326465e8db2edc96a14ab60655e0
Niko <niko0728.it@gmail.com>
2023-01-27 12:29:21 +1100

    Setting up the connection

--- DBconnect.js
+++ DBconnect.js
@@ -1,18 +1,17 @@
 // //mongoDb connection
 require("dotenv").config();
 const mongoose = require("mongoose");
-let uri;
+// let uri;
 
-//Check if we are in prod or dev
-if (process.env.NODE_ENV === "development") {
-    uri = process.env.MONGO_URI_DEV;
-}
+// //Check if we are in prod or dev
+// if (process.env.NODE_ENV === "development") {
+//     uri = process.env.MONGO_URI_DEV;
+// }
 
-if (process.env.NODE_ENV === "production") {
-    uri = process.env.MONGO_URI_PROD;
-}
-
-
+// if (process.env.NODE_ENV === "production") {
+//     uri = process.env.MONGO_URI_PROD;
+// }
+const uri = 'mongodb+srv://Niko:233@cluster0.ela4eij.mongodb.net/?retryWrites=true&w=majority'
 // Mongo DB conncetion
 mongoose
     .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
