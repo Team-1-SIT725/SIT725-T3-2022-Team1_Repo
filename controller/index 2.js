0bee9865ae2f7c5ed4440d0b5d05fc584ee145df
Niko <niko0728.it@gmail.com>
2023-02-02 10:19:59 +1100

    update readme

---
+++ controller/index 2.js
@@ -0,0 +1,10 @@
+module.exports = {
+  accountController: require("./accountController"),
+  adminController: require("./adminController"),
+  itemController: require("./itemController"),
+  itemSearchController: require("./itemSearchController"),
+  messagesController: require("./messagesController"),
+  userProfilePrivateController: require("./userProfilePrivateController"),
+  userProfilePublicController: require("./userProfilePublicController"),
+  addressVarController: require("./addressVarController"),
+};
